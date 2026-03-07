ec40d58e3a92ec94ef85f1a671e7233f717f7cd4
Dewanshu-Chirkhe <167283813+Dewanshu-Chirkhe@users.noreply.github.com>
2026-03-07 17:16:03 +0530

    remove compiled files

---
+++ 800/goalsOfVictory.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class goalsOfVictory {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        sc.close();
+    }
+}
