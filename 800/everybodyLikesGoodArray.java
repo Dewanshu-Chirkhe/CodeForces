4ed47a498ae2680e5ae22e50e4bb44ff5f6fac30
Dewanshu-Chirkhe <167283813+Dewanshu-Chirkhe@users.noreply.github.com>
2026-03-16 22:08:02 +0530

    1777A

--- 800/everybodyLikesGoodArray.java
+++ 800/everybodyLikesGoodArray.java
@@ -2,30 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class everybodyLikesGoodArray {
-    public static boolean isGood(List<Integer> list){
-        int n = list.size();
-        for(int i=0 ; i<n-1 ; i++){
-            int curr = list.get(i);
-            int next = list.get(i+1);
-            if((curr % 2 == 0 && next % 2 == 0) || (curr % 2 != 0 && next % 2 != 0))
-                return false;
-        }
-        return true;
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         while(t-- > 0){
             int n = sc.nextInt();
-            List<Integer> list = new ArrayList<>();
-            for(int i=0 ; i<n ; i++){
-                list.add(sc.nextInt());
+            int[] arr = new int[n];
+
+            for(int i = 0; i < n; i++){
+                arr[i] = sc.nextInt();
             }
 
             int operations = 0;
-            while(!isGood(list)){
-                operations++;
-                
+
+            for(int i = 0; i < n - 1; i++){
+                if(arr[i] % 2 == arr[i+1] % 2){
+                    operations++;
+                }
             }
 
             System.out.println(operations);
