b62942c9b102f77b4009fbd6d0c9adb4d1d382f9
Dewanshu-Chirkhe <167283813+Dewanshu-Chirkhe@users.noreply.github.com>
2026-03-30 21:47:00 +0530

    1726A

---
+++ 900/mainakAndArray.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class mainakAndArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i=0 ; i<n ; i++){
+                arr[i] = sc.nextInt();
+            }
+
+            int ans = 0;
+
+            for (int i = 1; i < n; i++) {
+                ans = Math.max(ans, arr[i] - arr[0]);
+            }
+
+            for (int i = 0; i < n - 1; i++) {
+                ans = Math.max(ans, arr[n - 1] - arr[i]);
+            }
+
+            for (int i = 0; i < n - 1; i++) {
+                ans = Math.max(ans, arr[i] - arr[i + 1]);
+            }
+
+            System.out.println(ans);
+        }
+        sc.close();
+    }
+}
