85bbfb4fb2ee4180cceb232332386eef795815b6
Dewanshu-Chirkhe <167283813+Dewanshu-Chirkhe@users.noreply.github.com>
2026-04-01 20:50:12 +0530

    1679A

---
+++ 900/avtoBus.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class avtoBus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            long n = sc.nextLong();
+            if(n < 4 || n % 2 != 0) System.out.println(-1);
+            else{
+                long min = (n + 5) / 6;
+                long max = n / 4;
+
+                System.out.println(min+" "+max);
+            }
+        }
+        sc.close();
+    }
+}
