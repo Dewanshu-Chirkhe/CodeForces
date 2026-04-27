d8e319bd0bdef7fa89ba1052fe0c3fcbcb16bf05
Dewanshu-Chirkhe <167283813+Dewanshu-Chirkhe@users.noreply.github.com>
2026-04-27 19:09:57 +0530

    Codeforces: 1471A

--- 900/StrangePartition_1471A.java
+++ 900/StrangePartition_1471A.java
@@ -7,20 +7,17 @@ public class StrangePartition_1471A {
         while(t-- > 0){
             int n = sc.nextInt();
             int x = sc.nextInt();
-            int[] arr = new int[n];
-            
-            int sum = 0;
-            int div = 0;
+            long[] arr = new long[n];
             for(int i=0 ; i<n ; i++){
-                arr[i] = sc.nextInt();
-                sum += arr[i];
-                div += Math.ceilDiv(arr[i], x);
+                arr[i] = sc.nextLong();
             }
             
-            sum /= x;
-            
-            int min = Math.min(sum, div);
-            int max = Math.max(sum, div);
+            long max = 0, min = 0;
+            for(int i=0 ; i<n ; i++){
+                max += Math.ceilDiv(arr[i], x);
+                min += arr[i];
+            }
+            min = Math.ceilDiv(min, x);
 
             System.out.println(min+" "+max);
         }
